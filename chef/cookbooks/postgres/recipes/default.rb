c4c16ecbeda56ee04aaa01ed58c54e147fe5fb6d
wanderanimrod <wanderanimrod@gmail.com>
2014-09-10 12:42:50 +0300

    [nim] #X Installs and configures PosgteSQL

--- chef/cookbooks/postgres/recipes/default.rb
+++ chef/cookbooks/postgres/recipes/default.rb
@@ -1,8 +1,27 @@
-#
-# Cookbook Name:: postgres
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+execute "Create default locales for postgres server to use when starting" do
+	command "export LANGUAGE=en_US.UTF-8 && export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && locale-gen en_US.UTF-8 && dpkg-reconfigure locales"
+	action :run
+end
+
+package "postgresql-9.2.4" do
+	action :install
+end
+
+template "/etc/postgresql/9.1/main/pg_hba.conf" do
+  source "pg_hba.conf.erb"
+end
+
+template "/etc/postgresql/9.1/main/postgresql.conf" do
+  source "postgresql.conf.erb"
+end
+
+service "postgresql" do
+  action :restart
+end
+
+execute "Create empty database" do
+	command "createdb eums"
+	user "postgres"
+	not_if "psql --list | grep eums", :user => 'postgres'
+	action :run
+end 
