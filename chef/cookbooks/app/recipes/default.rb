0e720f341e7683aebb7790bcde0247350bddf401
wanderanimrod <wanderanimrod@gmail.com>
2014-09-10 10:10:43 +0300

    [nim] #X Clones eums app

--- chef/cookbooks/app/recipes/default.rb
+++ chef/cookbooks/app/recipes/default.rb
@@ -1,8 +1,15 @@
-#
-# Cookbook Name:: app
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+directory "/home/eums/app" do
+	mode 0755
+	owner "eums"
+	group "eums"
+	action :create
+end
+
+git "/home/eums/app" do
+	user "eums"
+	repository "https://github.com/unicefuganda/eums.git"
+	reference "master"
+	revision "HEAD"
+	action "sync"
+end
+
