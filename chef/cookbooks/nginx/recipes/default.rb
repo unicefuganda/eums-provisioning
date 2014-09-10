5d75dab4ece6060dfe4b5e804943b4c8bedbb24d
wanderanimrod <wanderanimrod@gmail.com>
2014-09-10 12:41:55 +0300

    [nim] #X Adds recipe to install and configure nginx

--- chef/cookbooks/nginx/recipes/default.rb
+++ chef/cookbooks/nginx/recipes/default.rb
@@ -1,8 +1,15 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+package "nginx" do
+	action :install
+end
+
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+end
+
+template "/etc/nginx/conf.d/eums-server.conf" do
+  source "eums-server.conf.erb"
+end
+
+service "nginx" do
+  action :restart
+end
