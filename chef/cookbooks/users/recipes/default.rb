38558962b39eae6838c9377db2e906b861978ea5
wanderanimrod <wanderanimrod@gmail.com>
2014-09-10 12:43:34 +0300

    [nim] #X Corrects indentation

--- chef/cookbooks/users/recipes/default.rb
+++ chef/cookbooks/users/recipes/default.rb
@@ -1,19 +1,20 @@
 user "eums" do
-        action :create
-        home "/home/eums"
-        system true
-        password "$1$.WsplVWP$vZGPs6vxS0FmQ0dh83M541"
+  action :create
+  home "/home/eums"
+  system true
+  #remove this.
+  password "$1$.WsplVWP$vZGPs6vxS0FmQ0dh83M541"
 end
 
 user "eums" do
-        action :modify
-        home "/home/eums"
-        supports :manage_home=>true
+  action :modify
+  home "/home/eums"
+  supports :manage_home=>true
 end
 
 directory "/home/eums" do   
-        owner "eums"
-        group "eums"
-        mode "0755"
-        action :create
+  owner "eums"
+  group "eums"
+  mode "0755"
+  action :create
 end
