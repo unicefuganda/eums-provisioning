6d211ddb831f25935866c409e97b2d69c7682379
Kelvin Smith <oneKelvinSmith@gmail.com>
2014-10-16 13:51:30 +0300

    [kelvin, james] #55 remove rapidpro_auth cookbook

--- chef/cookbooks/rapidpro_auth/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-execute "Dummy rapid pro token" do
-	user "root"
-	command "echo \"RAPIDPRO_API_TOKEN=''\" > /etc/environment"
-end
-
-bash "Source bashrc" do
-	user "root"
-	command "source /etc/environment"
-end
