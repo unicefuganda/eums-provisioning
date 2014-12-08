5b8c55dc995f2f0445b9a5b9c6ef438a0aefc146
James Oloo Onyango <jamesony@thoughtworks.com>
2014-12-08 15:44:25 +0300

    [nim, james] #X fixed recipes to run db migrations

--- chef/cookbooks/init_db/recipes/default.rb
+++ chef/cookbooks/init_db/recipes/default.rb
@@ -1,25 +1,25 @@
-bash "Migrate database" do
+execute "Migrate database" do
   user "eums"
   cwd "/home/eums/app"
-    command "source /home/eums/virtualenv/eums/bin/activate && ./manage.py migrate --noinput"
-    action :run
+  command "/home/eums/virtualenv/eums/bin/python manage.py migrate"
+  action :run
 end
 
 template "/home/eums/app/eums/signals/handlers.py" do
   source "pre_seed_handlers.py.erb"
 end
 
-bash "Seed showcase data" do
+execute "Seed showcase data" do
     user "eums"
     cwd "/home/eums/app"
-    command "source /home/eums/virtualenv/eums/bin/activate && ./manage.py loaddata eums/fixtures/show_case_data.json"
+    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/show_case_data.json"
     action :run
 end
 
-bash "Seed flows and questions" do
+execute "Seed flows and questions" do
     user "eums"
     cwd "/home/eums/app"
-    command "source /home/eums/virtualenv/eums/bin/activate && ./manage.py loaddata eums/fixtures/flows_and_questions.json"
+    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/flows_and_questions.json"
     action :run
 end
 
