a1b06edf9853b31bf4397f0a743dc037493626b7
James Oloo Onyango <jamesony@thoughtworks.com>
2014-12-08 19:24:20 +0300

    [nim, james] #X loading updated data specific for staging

--- chef/cookbooks/init_db/recipes/default.rb
+++ chef/cookbooks/init_db/recipes/default.rb
@@ -9,17 +9,10 @@ template "/home/eums/app/eums/signals/handlers.py" do
   source "pre_seed_handlers.py.erb"
 end
 
-execute "Seed showcase data" do
+execute "Load sample data" do
     user "eums"
     cwd "/home/eums/app"
-    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/show_case_data.json"
-    action :run
-end
-
-execute "Seed flows and questions" do
-    user "eums"
-    cwd "/home/eums/app"
-    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/flows_and_questions.json"
+    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/sample-data.json"
     action :run
 end
 
