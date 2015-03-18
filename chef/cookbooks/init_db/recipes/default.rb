af3d616103e1a2eec685919ab139ad80b396d86e
Neil Coutinho <ncoutin@thoughtworks.com>
2015-03-18 17:45:43 +0300

    Change load data to use new deployment fixture.

--- chef/cookbooks/init_db/recipes/default.rb
+++ chef/cookbooks/init_db/recipes/default.rb
@@ -9,10 +9,10 @@ template "/home/eums/app/eums/signals/handlers.py" do
   source "pre_seed_handlers.py.erb"
 end
 
-execute "Load sample data" do
+execute "Load new deployment instance data" do
     user "eums"
     cwd "/home/eums/app"
-    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/sample-data.json"
+    command "/home/eums/virtualenv/eums/bin/python manage.py loaddata eums/fixtures/new-deployment-instance.json"
     action :run
 end
 
